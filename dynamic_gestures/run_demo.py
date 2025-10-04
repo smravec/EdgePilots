a12b852ec5ee10765d0587bea5c3e368e3e5bdc6
Šimon Mravec <simon.mravec@gmail.com>
2025-10-04 12:26:45 +0300

    Outputs direct commands

--- dynamic_gestures/run_demo.py
+++ dynamic_gestures/run_demo.py
@@ -5,7 +5,7 @@ import cv2
 import numpy as np
 
 from main_controller import MainController
-from utils import Drawer, Event, targets
+from utils import targets
 
 
 def run(args):
@@ -14,8 +14,10 @@ def run(args):
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
     controller = MainController(args.detector, args.classifier)
-    drawer = Drawer()
     debug_mode = args.debug
+    prev_commamnd = None
+    command = ""
+
     while cap.isOpened():
         ret, frame = cap.read()
         frame = cv2.flip(frame, 1)
@@ -28,7 +30,20 @@ def run(args):
                     for i in range(bboxes.shape[0]):
                         box = bboxes[i, :]
                         gesture = targets[labels[i]] if labels[i] is not None else "None"
-                        
+
+                    
+                        if gesture == "fist":
+                            command = "STOP"
+                        elif gesture == "three_gun":
+                            command = "SHOOT"
+                        elif gesture == "palm":
+                            command = "MOVE"
+
+                        #print(gesture)
+                        if command != prev_commamnd:
+                            print(command)
+                        prev_commamnd = command
+
                         cv2.rectangle(frame, (box[0], box[1]), (box[2], box[3]), (255, 255, 0), 4)
                         cv2.putText(
                             frame,
@@ -42,90 +57,7 @@ def run(args):
 
                 fps = 1.0 / (time.time() - start_time)
                 cv2.putText(frame, f"fps {fps:.2f}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-            if len(controller.tracks) > 0:
-                count_of_zoom = 0
-                thumb_boxes = []
-                for trk in controller.tracks:
-                    if trk["tracker"].time_since_update < 1:
-                        if len(trk['hands']):
-                            count_of_zoom += (trk['hands'][-1].gesture == 3)
 
-                            thumb_boxes.append(trk['hands'][-1].bbox)
-                            if len(trk['hands']) > 3 and [trk['hands'][-1].gesture, trk['hands'][-2].gesture, trk['hands'][-3].gesture] == [23, 23, 23]:
-                                x, y, x2, y2 = map(int, trk['hands'][-1].bbox)
-                                x, y, x2, y2 = max(x, 0), max(y, 0), max(x2, 0), max(y2, 0)
-                                bbox_area = frame[y:y2, x:x2]
-                                blurred_bbox = cv2.GaussianBlur(bbox_area, (51, 51), 10)
-                                frame[y:y2, x:x2] = blurred_bbox
-
-                        if trk["hands"].action is not None:
-                            if Event.SWIPE_LEFT == trk["hands"].action or  Event.SWIPE_LEFT2 == trk["hands"].action or  Event.SWIPE_LEFT3 == trk["hands"].action:
-                                drawer.set_action(trk["hands"].action)
-                                trk["hands"].action = None
-                                ...
-                            elif Event.SWIPE_RIGHT == trk["hands"].action or Event.SWIPE_RIGHT2 == trk["hands"].action or Event.SWIPE_RIGHT3 == trk["hands"].action:
-                                drawer.set_action(trk["hands"].action)
-                                trk["hands"].action = None
-                                ...
-                            elif Event.SWIPE_UP == trk["hands"].action or Event.SWIPE_UP2 == trk["hands"].action or Event.SWIPE_UP3 == trk["hands"].action:
-                                drawer.set_action(trk["hands"].action)
-                                trk["hands"].action = None
-                                ...
-                            elif Event.SWIPE_DOWN == trk["hands"].action or Event.SWIPE_DOWN2 == trk["hands"].action or Event.SWIPE_DOWN3 == trk["hands"].action:
-                                drawer.set_action(trk["hands"].action)
-                                trk["hands"].action = None
-                                ...
-                            elif Event.DRAG == trk["hands"].action:
-                                drawer.set_action(trk["hands"].action)
-                                ...
-                            elif Event.DROP == trk["hands"].action:
-                                drawer.set_action(trk["hands"].action)
-                                trk["hands"].action = None
-                                ...
-                            elif Event.FAST_SWIPE_DOWN == trk["hands"].action:
-                                drawer.set_action(trk["hands"].action)
-                                trk["hands"].action = None
-                                ...
-                            elif Event.FAST_SWIPE_UP == trk["hands"].action:
-                                drawer.set_action(trk["hands"].action)
-                                trk["hands"].action = None
-                                ...
-                            elif Event.ZOOM_IN == trk["hands"].action:
-                                drawer.set_action(trk["hands"].action)
-                                trk["hands"].action = None
-                                ...
-                            elif Event.ZOOM_OUT == trk["hands"].action:
-                                drawer.set_action(trk["hands"].action)
-                                trk["hands"].action = None
-                                ...
-                            elif Event.DOUBLE_TAP == trk["hands"].action:
-                                drawer.set_action(trk["hands"].action)
-                                trk["hands"].action = None
-                                ...
-                            elif Event.DRAG2 == trk["hands"].action or Event.DRAG3 == trk["hands"].action:
-                                drawer.set_action(trk["hands"].action)
-                                ...
-                            elif Event.DROP2 == trk["hands"].action or Event.DROP3 == trk["hands"].action:
-                                drawer.set_action(trk["hands"].action)
-                                trk["hands"].action = None
-                                ...
-                            elif Event.TAP == trk["hands"].action:
-                                drawer.set_action(trk["hands"].action)
-                                trk["hands"].action = None
-                                ...
-                            elif Event.COUNTERCLOCK == trk["hands"].action:
-                                drawer.set_action(trk["hands"].action)
-                                trk["hands"].action = None
-                                ...
-                            elif Event.CLOCKWISE == trk["hands"].action:
-                                drawer.set_action(trk["hands"].action)
-                                trk["hands"].action = None
-                                ...
-                                
-                if count_of_zoom == 2:
-                    drawer.draw_two_hands(frame, thumb_boxes)
-            if debug_mode:
-                frame = drawer.draw(frame)
             cv2.imshow("frame", frame)
             if cv2.waitKey(1) & 0xFF == ord("q"):
                 break
